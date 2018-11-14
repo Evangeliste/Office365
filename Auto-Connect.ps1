4b4e0cea8f08a3e4d05a901b0ac6b9f371c44085
Evangelistr <alexis@inventiq.fr>
2018-11-14 13:49:49 +0100

    V1.1 GenCred
    Add GenCred capacities to the script

--- Auto-Connect.ps1
+++ Auto-Connect.ps1
@@ -5,71 +5,95 @@
 #		Script author : Alexis / Evangeliste
 #		mail 	: alexis@inventiq.fr
 #		Create 	: 27 February 2018
-#		Version	: 1.0
-#		UPDate 	:
+#		Version	: 1.1
+#		UPDate 	: 14 November 2018, Add genCred capacities
 #
 #########################################################################################################################
 
+<#     
+    .DESCRIPTION 
+        This script help you to connect to an Office 365 or Azure Tenant and load all module
+        
+    .EXAMPLE 
+        Generate your credential in XML File
+        .\Auto-Connect.ps1 -GenCred "YourPath"
+
+        Connection
+        .\Auto-Connect.ps1 -MyCred "YourPath"
+
+    Use : This script is provided as it and I accept no responsibility for any issues arising from its use. 
+#> 
+
+
 #Function/Module
 #########################################################################################################################
-[CmdletBinding(SupportsShouldProcess = $true)] 
-param ( 
-    [String]$MyCred
-    ) 
+[CmdletBinding(SupportsShouldProcess = $true)]
+param (
+    [String]$MyCred,
+    [String]$GenCred
+    )
 
-Import-Module MSOnline
-Import-Module AzureADPreview
-Import-Module LyncOnlineConnector
+IF ($GenCred -notlike $Null)
+    {
+    Get-Credential | Export-Clixml -Path $GenCred
+    }
+ELSE
+    {
+    Import-Module MSOnline
+    Import-Module AzureADPreview
+    Import-Module LyncOnlineConnector
 
-$CheckModule = Get-Module
-    IF ($CheckModule.name -eq "MSOnline")
-        {
-        Write-Host "Module MSOnline is OK ! Check next module" -ForegroundColor Green
-        }
-    else 
-        {
-        Write-Host "You do not have MSOnline requiered module installed ! Please make the necesarry" -ForegroundColor Red
-        Break
-        }
+    $CheckModule = Get-Module
+        IF ($CheckModule.name -eq "MSOnline")
+            {
+            Write-Host "Module MSOnline is OK ! Check next module" -ForegroundColor Green
+            }
+        else 
+            {
+            Write-Host "You do not have MSOnline requiered module installed ! Please make the necesarry" -ForegroundColor Red
+            Break
+            }
 
-    IF ($CheckModule.name -eq "AzureADPreview")
-        {
-        Write-Host "Module AzureAD is OK ! Check next module" -ForegroundColor Green
-        }
-    else 
-        {
-        Write-Host "You do not have AzureADPreview requiered module installed ! Please make the necesarry" -ForegroundColor Red
-        Break
-        }
+        IF ($CheckModule.name -eq "AzureADPreview")
+            {
+            Write-Host "Module AzureAD is OK ! Check next module" -ForegroundColor Green
+            }
+        else 
+            {
+            Write-Host "You do not have AzureADPreview requiered module installed ! Please make the necesarry" -ForegroundColor Red
+            Break
+            }
 
-Write-Host "All Module are OK !! Well done" $env:UserName "the script continue" -ForegroundColor White -BackgroundColor DarkGreen
-Start-Sleep 5
+    Write-Host "All Module are OK !! Well done" $env:UserName "the script continue" -ForegroundColor White -BackgroundColor DarkGreen
+    Start-Sleep 5
 
 
-Get-PsSession | Remove-PsSession
+    Get-PsSession | Remove-PsSession
 
-#Define variables
-#########################################################################################################################
+    #Define variables
+    #########################################################################################################################
 
-#Save your credential to an XML file : Get-Credential | Export-Clixml -Path C:\_Labs\_MyCred\YourCred.xml
-#$TenantCredentials = Import-Clixml "C:\_Labs\_MyCred\Demo.xml"
-$TenantCredentials = Import-Clixml $MyCred
+    #Save your credential to an XML file : Get-Credential | Export-Clixml -Path C:\_Labs\_MyCred\YourCred.xml
+    #$TenantCredentials = Import-Clixml "C:\_Labs\_MyCred\Demo.xml"
+    $TenantCredentials = Import-Clixml $MyCred
 
-#Connect to O365 Tenant
-#$TenantCredentials = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $TenantCredentials -Authentication Basic -AllowRedirection
-$CSSession = New-CsOnlineSession -Credential $TenantCredentials
-Import-PSSession $Session
-Import-PSSession $CSSession
-Connect-MsolService -Credential $TenantCredentials
-Connect-AzureAD -Credential $TenantCredentials
+    #Connect to O365 Tenant
+    #$TenantCredentials = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $TenantCredentials -Authentication Basic -AllowRedirection
+    Import-PSSession $Session
+    Connect-MsolService -Credential $TenantCredentials
+    Connect-AzureAD -Credential $TenantCredentials
 
-$DomainTenant = ""
-$DomainTenant = ((Get-MsolDomain | Where-Object {$_.Name -like "*onmicrosoft.com" -and $_.Name -notlike "*mail.onmicrosoft.com"}).Name).replace(".onmicrosoft.com","")
-#Clear-Host
+    $CSSession = New-CsOnlineSession -Credential $TenantCredentials
+    Import-PSSession $CSSession
 
-Write-Host "##########################################################################################################################" -ForegroundColor Green
-Write-Host "#                                                                                                                        #" -ForegroundColor Green
-Write-Host "#                                   You are now connected to" $DomainTenant.ToUpper() "Tenant                                              #" -ForegroundColor Green
-Write-Host "#                                                                                                                        #" -ForegroundColor Green
-Write-Host "##########################################################################################################################" -ForegroundColor Green
+    $DomainTenant = ""
+    $DomainTenant = ((Get-MsolDomain | Where-Object {$_.Name -like "*onmicrosoft.com" -and $_.Name -notlike "*mail.onmicrosoft.com"}).Name).replace(".onmicrosoft.com","")
+    #Clear-Host
+
+    Write-Host "##########################################################################################################################" -ForegroundColor Green
+    Write-Host "#                                                                                                                        #" -ForegroundColor Green
+    Write-Host "#                                   You are now connected to" $DomainTenant.ToUpper() "Tenant                                              #" -ForegroundColor Green
+    Write-Host "#                                                                                                                        #" -ForegroundColor Green
+    Write-Host "##########################################################################################################################" -ForegroundColor Green
+    }
