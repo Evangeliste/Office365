8d4436f5ca5d75eea23c792dd46138b6bb9603e9
Evangeliste <35897333+Evangeliste@users.noreply.github.com>
2020-11-19 10:52:47 +0100

    Update Auto-Connect.ps1

--- Auto-Connect.ps1
+++ Auto-Connect.ps1
@@ -5,8 +5,11 @@
 #		Script author : Alexis / Evangeliste
 #		mail 	: alexis@inventiq.fr
 #		Create 	: 27 February 2018
-#		Version	: 1.1
-#		UPDate 	: 14 November 2018, Add genCred capacities
+#		Version	: 1.3.0
+#		UPDate v1.1.0 : 14 November 2018, Add genCred capacities
+#       UPDate v1.2.0 : 20 November 2018, Add Microsoft Teams Module and change import session and connect order
+#       UPDate v1.3.0 : 19 December 2019, correction of connexion to SPO
+#       UPDate v1.4.0 : 26 September 2020, UPDate connexion for SfBO with Teams Module
 #
 #########################################################################################################################
 
@@ -30,7 +33,8 @@
 [CmdletBinding(SupportsShouldProcess = $true)]
 param (
     [String]$MyCred,
-    [String]$GenCred
+    [String]$GenCred,
+    $SfBO = $false
     )
 
 IF ($GenCred -notlike $Null)
@@ -41,9 +45,10 @@ ELSE
     {
     Import-Module MSOnline
     Import-Module AzureADPreview
-    Import-Module LyncOnlineConnector
+    #Import-Module LyncOnlineConnector
     Import-Module MicrosoftTeams
-    
+    Import-Module ExchangeOnlineManagement
+
     $errorActionPreference = "SilentlyContinue"
     
     $CheckModule = Get-Module
@@ -58,6 +63,16 @@ ELSE
             Break
             }
 
+        IF ($CheckModule.name -eq "ExchangeOnlineManagement")
+            {
+            Write-Host "Module ExchangeOnlineManagement is OK ! Check next module" -ForegroundColor Green
+            }
+        else 
+            {
+            Write-Host "You do not have ExchangeOnlineManagement requiered module installed ! Please make the necesarry" -ForegroundColor Red
+            Write-host "Please refer to https://docs.microsoft.com/en-us/powershell/exchange/exchange-online/exchange-online-powershell-v2/exchange-online-powershell-v2?view=exchange-ps for install this module" -ForegroundColor Red
+            }
+
         IF ($CheckModule.name -eq "AzureADPreview")
             {
             Write-Host "Module AzureAD is OK ! Check next module" -ForegroundColor Green
@@ -68,7 +83,8 @@ ELSE
             Write-host "Please refer to https://www.powershellgallery.com/packages/AzureADPreview for install this module" -ForegroundColor Red
             Break
             }
-            IF ($CheckModule.name -eq "MicrosoftTeams")
+
+        IF ($CheckModule.name -eq "MicrosoftTeams")
             {
             Write-Host "Module MicrosoftTeams is OK ! Check next module" -ForegroundColor Green
             }
@@ -79,38 +95,46 @@ ELSE
             Break
             }
 
-    Write-Host "All Module are OK !! Well done" $env:UserName "the script continue" -ForegroundColor White -BackgroundColor DarkGreen
+    Write-Host "All Module are OK !! Well done on" $env:COMPUTERNAME',' $env:UserName "the script continue" -ForegroundColor White -BackgroundColor DarkGreen
     Start-Sleep 5
-
 
     Get-PsSession | Remove-PsSession
 
     #Define variables
     #########################################################################################################################
 
-    #Save your credential to an XML file : Get-Credential | Export-Clixml -Path C:\_Labs\_MyCred\YourCred.xml
     $TenantCredentials = Import-Clixml $MyCred
 
-    #Connect to O365 Tenant
-    #$TenantCredentials = Get-Credential
-    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $TenantCredentials -Authentication Basic -AllowRedirection
-    Import-PSSession $Session
-
-    $CSSession = New-CsOnlineSession -Credential $TenantCredentials
-    Import-PSSession $CSSession
-
-    $TenantCredentials = Import-Clixml $MyCred
     Connect-MsolService -Credential $TenantCredentials
     Connect-AzureAD -Credential $TenantCredentials
     Connect-MicrosoftTeams -Credential $TenantCredentials
+    #Connect-ExchangeOnline -UserPrincipalName $TenantCredentials.UserName -ShowProgress $true
+    Connect-ExchangeOnline -Credential $TenantCredentials -ShowProgress $true
+    
 
+    IF ($SfBO -eq $True)
+        {
+        $CSSession = New-CsOnlineSession -Credential $TenantCredentials
+        Import-PSSession $CSSession
+        }
+    else 
+        {
+        Write-Host "SfBO module was not loaded" -ForegroundColor Yellow
+        }
+    
     $DomainTenant = ""
     $DomainTenant = ((Get-MsolDomain | Where-Object {$_.Name -like "*onmicrosoft.com" -and $_.Name -notlike "*mail.onmicrosoft.com"}).Name).replace(".onmicrosoft.com","")
     #Clear-Host
 
-    Write-Host "##########################################################################################################################" -ForegroundColor Green
-    Write-Host "#                                                                                                                        #" -ForegroundColor Green
-    Write-Host "#                                   You are now connected to" $DomainTenant.ToUpper() "Tenant                                              #" -ForegroundColor Green
-    Write-Host "#                                                                                                                        #" -ForegroundColor Green
-    Write-Host "##########################################################################################################################" -ForegroundColor Green
+    #Connect to SharePoint Online (SPO)
+    #########################################################################################################################
+    $SPOURI = "https://"+$DomainTenant+"-admin.sharepoint.com"
+    Connect-SPOService -Url $SPOURI -Credential $TenantCredentials
+
+    Write-Host ""
+    Write-Host "#########################################################################################################################" -ForegroundColor Green
+    Write-Host "#                                                                                                                        " -ForegroundColor Green
+    Write-Host "#                                   You are now connected to" $DomainTenant.ToUpper() "Tenant                            " -ForegroundColor Green
+    Write-Host "#                                                                                                                        " -ForegroundColor Green
+    Write-Host "#########################################################################################################################" -ForegroundColor Green
     }
