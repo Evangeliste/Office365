18ca52e1a01228ae097577511c3096fe022c0246
CHARRIERE Alexis (P) <alexis.charriere@ratp.fr>
2021-03-10 14:22:20 +0100

    Update Get-Delegate.ps1

--- Get-Delegate.ps1
+++ Get-Delegate.ps1
@@ -34,7 +34,7 @@ New-Item $FolderBuild -Type Directory -ErrorAction SilentlyContinue
 
 $MBXDelegateLog = $FolderBuild+"\"+$MyDateTime+"_ExportDelegation.csv"
 New-Item $MBXDelegateLog -type file | Out-Null
-"Identity,FolderName,User,AccessRights" | Tee-Object -FilePath $MBXDelegateLog -Append -ErrorAction SilentlyContinue
+"Identity;User;AccessRights" | Tee-Object -FilePath $MBXDelegateLog -Append -ErrorAction SilentlyContinue
 
 #Build
 #########################################################################################################################
@@ -46,9 +46,10 @@ $ImportRecipient = Get-Mailbox -ResultSize $ResultSize
 
 foreach ($mbx in $ImportRecipient)
     {
-    $Perm = Get-MailboxFolderPermission $mbx.PrimarySmtpAddress | Where-Object {$_.User -notlike "NT AUTHORITY\*"-and $_.User -notlike "Default" -and $_.User -notlike "Anonymous" -and $_.User -notlike "Utilisateur NT*" -and $_.AccessRights -ne "none"}
+    #$Perm = Get-MailboxPermission $mbx.PrimarySmtpAddress | Where-Object {$_.User -notlike "NT AUTHORITY\*"-and $_.User -notlike "Default" -and $_.User -notlike "Anonymous" -and $_.User -notlike "Utilisateur NT*" -and $_.AccessRights -ne "none"}
+    $Perm = Get-MailboxPermission $mbx.PrimarySmtpAddress | Where-Object {$_.User -notlike "NT AUTHORITY\*" -and $_.User -notlike "Default" -and $_.User -notlike "*Management" -and $_.User -notlike "*Exchange*" -and $_.User -notlike "Anonymous" -and $_.User -notlike "Utilisateur NT*" -and $_.User -notlike "S-1-5-21-*" -and $_.User -notlike "*\Admin*" -and $_.User -notlike "*AC738250ADM" -and $_.User -notlike "*CA177404" -and $_.User -notlike "INFRAWIN\Delegated*" -and $_.User -notlike "INFRAWIN\Managed*"}
     IF ($Perm) 
         {
-        $($mbx.PrimarySmtpAddress)+";"+$Perm.FolderName+";"+$Perm.User+";"+$Perm.AccessRights | Tee-Object -FilePath $MBXDelegateLog -Append | Write-Host -ForegroundColor Green
+        $($mbx.PrimarySmtpAddress)+";"+$Perm.User+";"+$Perm.AccessRights | Tee-Object -FilePath $MBXDelegateLog -Append | Write-Host -ForegroundColor Green
         }
     }
